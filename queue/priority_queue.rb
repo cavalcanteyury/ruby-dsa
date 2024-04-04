23a65ca2e9177ad6cdc676e7b5a505448796a31b
cavalcanteyury <yurypcfarias@gmail.com>
2024-04-04 18:47:09 -0300

    little to no refactor

--- queue/priority_queue.rb
+++ queue/priority_queue.rb
@@ -1,12 +1,5 @@
 #!/usr/bin/env ruby
 
-# build PriorityQueue
-# Insert into priority queue
-# get min/max
-# extract min/max
-# clear
-#
-
 class Element
   attr_accessor :data, :priority
 
@@ -14,45 +7,62 @@ class Element
     @data, @priority = data, priority
   end
 
-  def <=>(elem)
-    @priority <=> elem.priority
+  # [Element <=> Element]
+  # spaceship operator:
+  # a <=> b
+  # if a lesser than b returns -1
+  # if a greater than b = 1
+  # if a equals b = 0
+  # if a not comparable with b = nil
+  def <=>(element)
+    @priority <=> element.priority
   end
 end
 
 class PriorityQueue
   attr_accessor :elements
 
-  def initialize(elements = [], order = -1)
+  def initialize(elements, heap_order = -1)
     @elements = elements
-    @compare = lambda { |left_el, right_el| (left_el <=> right_el) == order}
+    @compare = lambda { |x, y| (x <=> y) == heap_order }
+  end
+
+  def last_index
+    @elements.size - 1
+  end
+
+  def size
+    @elements.size
   end
 
   def <<(element)
     @elements << element
 
-    target_index = @elements.size - 1
+    target_index = last_index
     parent_index = (target_index - 1) / 2
 
     while @compare.call(@elements[target_index], @elements[parent_index])
       swap(target_index, parent_index)
 
       target_index = parent_index
-
       parent_index = (target_index - 1) / 2
     end
   end
 
   def pop
-    @elements[0], @elements[@elements.size - 1] = @elements[@elements.size - 1], @elements[0]
+    swap(0, last_index)
 
     @elements.pop
 
     target_index = 0
-    left_index = 2 * target_index + 1
-    right_index = 2 * target_index + 2
+    left_index = (target_index * 2) + 1
+    right_index = (target_index * 2) + 2
 
-    while (left_index < @elements.size && @compare.call(@elements[left_index], @elements[target_index])) || (right_index < @elements.size && @compare.call(@elements[right_index], @elements[target_index]))
-      smallest_index = if right_index >= @elements.size || @elements[left_index].data < @elements[right_index].data
+    while (
+      left_index < size && @compare.call(@elements[left_index], @elements[target_index]) ||
+      right_index < size && @compare.call(@elements[right_index], @elements[target_index])
+    )
+      smallest_index = if right_index >= size || @elements[left_index].data < @elements[right_index].data
                          left_index
                        else
                          right_index
@@ -61,59 +71,53 @@ class PriorityQueue
       swap(target_index, smallest_index)
 
       target_index = smallest_index
-      left_index = 2 * target_index + 1
-      right_index = 2 * target_index + 2
+      left_index = (target_index * 2) + 1
+      right_index = (target_index * 2) + 2
     end
   end
 
-  def swap(target_index, parent_index)
-    @elements[parent_index], @elements[target_index] = @elements[target_index], @elements[parent_index]
+  def swap(target_index, swap_index)
+    @elements[target_index], @elements[swap_index] = @elements[swap_index], @elements[target_index]
   end
 
   def inspect
     res = []
+
     for el in @elements
       res.push(el.data)
     end
 
     res
   end
-
-  # def pop
-  #   last_el_index = @elements.size - 1
-  #   @elements.sort!
-  #   @elements.delete_at(last_el_index)
-  # end
 end
 
+
+# Example usage
 min_heap = []
 
-min_heap << Element.new(4)
-min_heap << Element.new(8)
-min_heap << Element.new(6)
+min_heap << Element.new(5)
 min_heap << Element.new(9)
-min_heap << Element.new(12)
 min_heap << Element.new(7)
 min_heap << Element.new(10)
+min_heap << Element.new(13)
+min_heap << Element.new(8)
+min_heap << Element.new(11)
 
-pq = PriorityQueue.new(min_heap)
+pqueue = PriorityQueue.new(min_heap)
 
-p pq.inspect()
+p pqueue.inspect()
 
-pq << Element.new(5)
+pqueue << Element.new(6)
 
-p pq.inspect()
+p pqueue.inspect
 
-pq.pop()
+pqueue.pop()
+pqueue.pop()
 
-p pq.inspect()
+p pqueue.inspect
 
-pq.pop()
+pqueue.pop()
+pqueue.pop()
+pqueue.pop()
 
-p pq.inspect()
-
-pq.pop()
-pq.pop()
-pq.pop()
-
-p pq.inspect()
+p pqueue.inspect
